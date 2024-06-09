887e9098431d05851e757201681c71a2dea4e56b
fedorez <fedorez@mail.ru>
2024-06-09 21:03:12 +0300

    Initial Commit

---
+++ FDCReverse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FDCReverse
+//
+//  Created by Denis Fedorets on 09.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FDCReverse/FDCReverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDCReverseApp.swift
+//  FDCReverse
+//
+//  Created by Denis Fedorets on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FDCReverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
